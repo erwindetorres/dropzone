22902614bf15eb666557174f4b19f0b9642c9da7
Kira <thenapster.project@gmail.com>
2016-09-27 23:31:15 +0800

    Dropzone js implementation

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<meta charset="utf-8">
+
+<title>Dropzone Upload Files</title>
+
+<script src="js/dropzone.js"></script>
+<link rel="stylesheet" href="css/dropzone.css">
+<link rel="stylesheet" href="css/eric_meyer_style.css">
+
+<section>
+
+	<h1 id="try-it-out">Dragzone File Upload</h1>
+
+	<div id="dropzone">
+		<form action="upload.php" class="dropzone needsclick" id="demo-upload">
+			<div class="dz-message needsclick">
+				Drop files here or click to upload.<br />
+			</div>
+		</form>
+	</div>
+
+</section>
